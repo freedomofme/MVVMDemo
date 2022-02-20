6ec6ce949e8403b489d70ac0672bffe8553820f7
yel huang <xiegeixiong@gmail.com>
2022-02-20 20:09:15 +0800

    [feature]  edit readme and add internet permission

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # MVVMDemo
+
+
+## Cat Image Gallery
+
+### Architecture
+
+### CATS API
+
+#### source
+
+[https://docs.thecatapi.com/](https://docs.thecatapi.com/)
+
+
+
+
+
+
--- base/src/main/java/com/learn/base/Config.java
+++ base/src/main/java/com/learn/base/Config.java
@@ -1,4 +1,8 @@
 package com.learn.base;
 
 public class Config {
+    public static final String BASE_URL = "https://api.thecatapi.com/";
+
+    // not safe, just for demonstrate
+    public static final String TOKEN = "a1bc7b77-ef33-4fb7-b57c-7396e3c2d5e3";
 }
